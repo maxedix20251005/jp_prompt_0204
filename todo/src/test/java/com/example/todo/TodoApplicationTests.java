6df0411cf0b977ceea50d0edbc4a12a47e70f2e2
LAPTOP-EGH8ETNI\academia <202510_aw_syukuri@x-ide.sakura.ne.jp>
2026-02-04 12:25:14 +0900

    Bulk commit

---
+++ todo/src/test/java/com/example/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package todo.src.test.java.com.example.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
